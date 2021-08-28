877e1fc7458fc78a6e57cdf05e67cacd8e655b86
Hikaru Inomata <inomatahi@icloud.com>
2021-08-28 23:46:11 +0900

    p146まで

--- chapter-2/src/index.ts
+++ chapter-2/src/index.ts
@@ -8,3 +8,39 @@ let e = {type: 'ficus'}
 let f = [1, false]
 const g = [3]
 let h = null
+
+function sumVariadic(): number {
+  return Array
+    .from(arguments)
+    .reduce((total, n) => total + n, 0)
+}
+
+function sumVariadicSafe(...numbers: number[]): number {
+  return numbers.reduce((total, n) => total + n, 0)
+}
+
+type Unit = 'EUR' | 'GBP' | 'JPY' | 'USD'
+
+type Currency = {
+  unit: Unit
+  value: number
+}
+
+let Currency = {
+  from(value: number, unit: Unit): Currency {
+    return {
+      unit: unit,
+      value
+    }
+  }
+}
+
+function tuple<
+  T extends unknown[]
+>(
+  ...ts: T
+): T {
+  return ts
+}
+
+let aa = tuple(1, true)
